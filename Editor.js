1b689a2b0f47e467d9cf58b2ebbb0e791c451aa9
José Miguel Tabuyo <josetabuyo@gmail.com>
2013-08-28 20:46:25 -0300

    prueba touch

--- Editor.js
+++ Editor.js
@@ -1,12 +1,28 @@
+
 var onDeviceReady = function() {         
     var clienteHTTP = new NodoClienteHTTP('http://router-vortex.herokuapp.com', 1000);             
     NodoRouter.instancia.conectarBidireccionalmenteCon(clienteHTTP);
     
-	document.addEventListener('touchstart', function(e){
-		alert(e.screenX + ' ' + e.screenY);
-	}, false);
+	var gnStartX = 0;
+	var gnStartY = 0;
+	var gnEndX = 0;
+	var gnEndY = 0;
 	
 	
+	window.addEventListener('touchstart',function(event) {
+	  gnStartX = event.touches[0].pageX;
+	  gnStartY = event.touches[0].pageY;
+	},false);
+
+	window.addEventListener('touchmove',function(event) {
+	  gnEndX = event.touches[0].pageX;
+	  gnEndY = event.touches[0].pageY;
+	},false);
+
+	window.addEventListener('touchend',function(event) {
+	  alert('START (' + gnStartX + ', ' + gnStartY + ')   END (' + gnEndX + ', ' + gnEndY + ')');
+	},false);
+
 };
 
 $(document).ready(function() {  
